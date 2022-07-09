15c998e66ae0e3cc33208966d765325c78260dca
Eduardo Melo <nascimentomeloedu@gmail.com>
2022-07-08 23:04:48 -0300

    connection factory updated

--- src/Factory/ConnectionFactory.java
+++ src/Factory/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class ConnectionFactory {
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "EduApple#0104";
+    private static final String PASSWORD = "pass";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/agenda";
 
     public static Connection createConnectionToMySQL() throws Exception {
